4d2a1063bbf82d585282f0571c404083dec6c469
Jerry Sun <sunyiran0817@gmail.com>
2026-04-29 17:18:15 -0700

    Add Apache 2.0 license headers

--- src/solvers/heuristic_solver.cpp
+++ src/solvers/heuristic_solver.cpp
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2026 Yiran (Jerry) Sun, Zigang (Richard) Sun and contributors
+ *
+ * Licensed under the Apache License, Version 2.0.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ */
+
 #include "solver.h"
 
 #include "cost_model.h"
